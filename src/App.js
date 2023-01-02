f5e67acafe678dbbdfd6be0a1a408936e4f75977
praneetwalvekar33 <praneetwalvekar@gmail.com>
2023-01-02 21:08:02 +0530

    fix- changed the url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './searchicon.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com?apikey=eb681675';
+const API_URL = 'https://www.omdbapi.com?apikey=eb681675';
 
 const App = () => {
 
